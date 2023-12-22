074e5c72e900a0cc6d5be6c7ebc7d648e8509411
Sheriff Joe Excel <realexcel2020@gmail.com>
2023-12-23 00:20:58 +0100

    updated mongoose package

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://di7bw486wt2dc.cloudfront.net";
+export const host = "https://snappy.myakuracy.click";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
